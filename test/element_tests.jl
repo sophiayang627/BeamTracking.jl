bcd1388bb2db12666ebb74d5da51094d14994ca6
dtabell <dtabell@users.noreply.github.com>
2024-09-23 12:43:47 -0600

    Initialize testing harness.

---
+++ test/element_tests.jl
@@ -0,0 +1,24 @@
+using BeamTracking
+using AcceleratorLattice
+using Test
+
+# define elements
+@ele start = BeginningEle(pc_ref = 10.e6, species_ref = species("electron"));
+@ele d1 = Drift(L = 2.);
+
+# define beamlines
+bl_drift = beamline("bl_drift", [ start, d1 ]);
+
+# expand beamlines
+lat_drift = expand("lat_drift", [ bl_drift ])
+
+# test individual elements
+@testset "element_tests" begin
+  # drift
+  @test lat_drift["d1"][1].L == 2.0
+  #@test track!(d1, zf, zi) == [xz, pxf, yf, pyf, zf, pzf]
+
+  # quadrupole
+
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,13 @@
 using BeamTracking
-using Test, Distributions
+using Distributions
+using Test
 
 @testset "BeamTracking.jl" begin
     beam0 = Beam(10000, d_x=Normal(0, 1e-3), d_px=Normal(0, 1e-4), d_pz=Normal(0, 1e-2))
     beamf = Beam(10000)
+
+  @testset "element_tests" begin
+    include("element_tests.jl")
+  end
+
 end
