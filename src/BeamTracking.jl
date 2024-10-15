687c1e3246a86927fec132bcf4786f546513a8b4
mattsignorelli <113714234+mattsignorelli@users.noreply.github.com>
2024-10-15 13:07:43 -0400

    Update BeamTracking.jl

--- src/BeamTracking.jl
+++ src/BeamTracking.jl
@@ -38,12 +38,12 @@ end
 
 struct Beam{S,T}
   species::Species
-  beta_0::S
+  beta_gamma_0::S
   z::Coords{T}
 end
 
 function Beam(
-  n::Integer; species::Species=Species("electron"), beta_0=1,
+  n::Integer; species::Species=Species("electron"), beta_gamma_0=1,
   d_x::Distribution=Normal(0,0), d_px::Distribution=Normal(0,0), 
   d_y::Distribution=Normal(0,0), d_py::Distribution=Normal(0,0), 
   d_z::Distribution=Normal(0,0), d_pz::Distribution=Normal(0,0),
@@ -51,14 +51,14 @@ function Beam(
 
   coords = Coords(n; d_x=d_x, d_px=d_px, d_y=d_y, d_py=d_py, d_z=d_z, d_pz=d_pz)
 
-  return Beam(species, beta_0, coords)
+  return Beam(species, beta_gamma_0, coords)
 end
 
 # Creates a Beam as identity GTPSA
-function Beam(d::Descriptor; species::Species=Species("electron"), beta_0=1)
+function Beam(d::Descriptor; species::Species=Species("electron"), beta_gamma_0=1)
   GTPSA.numvars(d) == 6 || error("Invalid GTPSA Descriptor! Number of variables must be equal to 6.")
   z = vars(d)
-  return Beam(species, beta_0, Coords([z[1]], [z[2]], [z[3]], [z[4]], [z[5]], [z[6]]))
+  return Beam(species, beta_gamma_0, Coords([z[1]], [z[2]], [z[3]], [z[4]], [z[5]], [z[6]]))
 end
 
 
