2061960a67376f9c8a86022eaadbd640380e4295
dtabell <dtabell@users.noreply.github.com>
2024-10-16 14:42:22 -0600

    Update comments to includes notes about the implementation.

--- src/utils.jl
+++ src/utils.jl
@@ -122,6 +122,17 @@ end
 
 Compute the unnormalized sinc function, ``\\operatorname{sincu}(z) = \\operatorname{sin}(z) / z``,
 with a correct treatment of the removable singularity at the origin.
+
+The function ``\\sin(z) / z = 1 - z^2 / 3! + z^4 / 5! - z^6 / 7! + \\cdots``.
+For real values of ``z \\in (-1,1)``, we can truncate this series just before the
+``k^\text{th}`` term, ``z^{2k} / (2k+1)!``, and the alternating nature of this series
+guarantees our error does not exceed the value of this first truncated term. It
+follows that if ``z^2 / 6 < \\varepsilon_\\text{machine}``, we may simply truncate to 1.
+On the otherhand, if we allow for complex values, we can no longer assume the series
+alternates, and we must use a more conservative threshold. A numerical exploration
+suggests that the sum of terms starting at the ``k^\\text{th}`` one is bounded by
+``|z|^{2k} / (2k)!``. It then follows that if ``|z|^2 / 2 < \\varepsilon_\\text{machine}``,
+we may truncate to 1.
 """
 function sincu(z::Number)
   threshold = sqrt(2eps())
@@ -136,6 +147,8 @@ end
 
 Compute the hyperbolic sinc function, ``\\operatorname{sinch}(z) = \\operatorname{sinh}(z) / z``,
 with a correct treatment of the removable singularity at the origin.
+
+See sincu for notes about implementation.
 """
 function sinch(z::Number)
   threshold = sqrt(2eps())
