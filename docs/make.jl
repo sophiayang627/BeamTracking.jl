7e1d87eb186ae5b529e62ed603209a273c44e06d
mattsignorelli <113714234+mattsignorelli@users.noreply.github.com>
2024-10-17 17:00:27 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,21 @@
 using BeamTracking
 using Documenter
 
-DocMeta.setdocmeta!(BeamTracking, :DocTestSetup, :(using BeamTracking); recursive=true)
-
 makedocs(;
-    modules=[BeamTracking],
     authors="mattsignorelli <mgs255@cornell.edu> and contributors",
     sitename="BeamTracking.jl",
-    format=Documenter.HTML(;
-        canonical="https://bmad-sim.github.io/BeamTracking.jl",
-        edit_link="main",
-        assets=String[],
-    ),
+  format=Documenter.HTMLWriter.HTML(size_threshold = nothing),
+  pages = 
+  [
+    "Home" => "index.md",
+  ]
+)
+
     pages=[
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/bmad-sim/BeamTracking.jl",
-    devbranch="main",
+    repo="github.com/bmad-sim/BeamTracking.jl.git",
 )
