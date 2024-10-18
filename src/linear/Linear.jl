8653f00e7c8002b450fa92b7586b60c19c9032c4
Sophia Yang <xy466@cornell.edu>
2024-10-18 15:30:41 -0400

    added drift+quad

--- src/linear/Linear.jl
+++ src/linear/Linear.jl
@@ -39,6 +39,7 @@ including higher-order energy effects.
 """
 function track!(ele::Linear.Drift, beamf::Beam, beami::Beam)
   @assert !(beamf === beami) "Aliasing beamf === beami not allowed!"
+  L = ele.L
   zi = beami.z
   zf = beamf.z
   
@@ -47,12 +48,39 @@ function track!(ele::Linear.Drift, beamf::Beam, beami::Beam)
   @. zf.px = zi.px
   @. zf.y  = zi.y+zi.py*L
   @. zf.py = zi.py
-  @. zf.z  = zi.z-L*((zi.px^2)+(zi.py^2))/(1.0+zi.pz)^2/2.0
+  @. zf.z  = zi.z-zi.pz*L
   @. zf.pz = zi.pz 
   end
   
   return beamf
 end
 
+function track!(ele::Linear.Quadrupole,beamf::Beam,beami::Beam,s::Float64)
+  @assert !(beamf === beami) "Aliasing beamf === beami not allowed!"
+  zi = beami.z
+  zf = beamf.z
+ 
+  q = beami.species.charge
+  pc_ref = sr_pc(q, beami.beta_gamma_0) #reference momentum 
+  r = q/pc_ref #rigidity
+  k1 = ele.B1 / r #quadrupole strengh
+
+  k = sqrt(abs(k1))
+  ks = k * s
+  cx = (cosh(ks) * (k1 >= 0) + cos(ks) * (k1 < 0))
+  sx = (sinh(ks) * (k1 >= 0) + sin(ks) * (k1 < 0))
+  cy = (cos(ks) * (k1 >= 0) + cosh(ks) * (k1 < 0))
+  sy = (sin(ks) * (k1 >= 0) + sinh(ks) * (k1 < 0))
+
+  @FastGTPSA! begin
+   @. zf.x  = zi.x * cx + zi.px * sx / k 
+   @. zf.px = ( -1 * (k1<0) + 1 * (k1>=0) ) zi.x * sx + zi.px * cx
+   @. zf.y = zi.y * cy + zi.py * sy
+   @. zf.py = ( 1 * (k1<0) - 1 * (k1>=0) )zi.y * k * sy + zi.py * cy
+   @. zf.z = zi.z + zi.pz * L 
+   @. zf.pz = zi.pz
+
+  end 
+end 
 
 end
