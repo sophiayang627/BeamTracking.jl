a8cd5b5740ff1f504ff05fb3dab15a3c72584522
mattsignorelli <mgs255@cornell.edu>
2024-11-08 15:57:59 -0500

    track! modular now and different argument order, small updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,6 @@ using Test
 @testset "BeamTracking.jl" begin
     beam0 = Beam(10000, d_x=Normal(0, 1e-3), d_px=Normal(0, 1e-4), d_pz=Normal(0, 1e-2))
     beamf = Beam(10000)
-
-  #=
-  @testset "element_tests" begin
-    include("element_tests.jl")
-  end
-  =#
 end
 
 include("linear.jl")
