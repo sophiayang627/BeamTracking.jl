271ce58ef2d7293ff8f825412b2fdd0fee78d8e2
dtabell <dtabell@users.noreply.github.com>
2025-01-09 15:12:58 -0700

    Update and correct quadrupole integrator.

--- src/MatrixKick/MatrixKick.jl
+++ src/MatrixKick/MatrixKick.jl
@@ -10,7 +10,7 @@ Base.@kwdef struct Drift{T}
 end
 
 Base.@kwdef struct Quadrupole{T}
-  L::T   # quadrupole length / m
+  L::T    # quadrupole length / m
   Bn1::T  # quadrupole gradient / (T·m^-1)
 end
 
@@ -56,7 +56,6 @@ end # function track!(::Beam, ::Drift)
 # This integrator uses the so-called Matrix-Kick-Matrix method to implement
 # an integrator accurate though second-order in the integration step-size.
 function track!(beam::Beam, ele::MatrixKick.Quadrupole; work=get_work(beam, Val{6}()))
-  @assert !(beamf === beami) "Aliasing beamf === beami not allowed!"
   L = ele.L
 
   # κ^2 (kappa-squared) := (q g / P0) / (1 + δ)
@@ -70,11 +69,9 @@ function track!(beam::Beam, ele::MatrixKick.Quadrupole; work=get_work(beam, Val{
   trackQuadK!( v, v_work, L)
   trackQuadMx!(v_work, v, k2_num, L / 2)
 
-
   v .= v_work
-
   return beam
-end # function track!(::Quadrupole)
+end # function track!(::Beam, ::Quadrupole)
 
 
 """
@@ -86,11 +83,11 @@ function trackQuadMx!(vf, vi, k2_num, s)
   focus   = k2_num >= 0  # horizontally focusing
   defocus = k2_num <  0  # horizontally defocusing
 
-  p = @. 1 + vi.pz    # reduced momentum, P/P0 = 1 + δ
+  p =  @. 1 + vi.pz    # reduced momentum, P/P0 = 1 + δ
   k2 = @. k2_num / p  # κ^2 for each particle
   ks = @. sqrt(abs(k2)) * s  # |κ|s
-  xp = @. v.px / p  # x'
-  yp = @. v.py / p  # y'
+  xp = @. vi.px / p  # x'
+  yp = @. vi.py / p  # y'
 
   cx =  @. focus * cos(ks)    + defocus * cosh(ks)
   cy =  @. focus * cosh(ks)   + defocus * cos(ks)
@@ -98,16 +95,16 @@ function trackQuadMx!(vf, vi, k2_num, s)
   sy =  @. focus * sinhc(ks)  + defocus * sincu(ks)
   sx2 = @. focus * sincu(2ks) + defocus * sinhc(2ks)
   sy2 = @. focus * sinhc(2ks) + defocus * sincu(2ks)
-  sxz = @. focus * sin(ks)^2  + defocus * sinh(ks)^2
-  syz = @. focus * sinh(ks)^2 + defocus * sin(ks)^2
+  sxz = @. focus * sin(ks)^2  - defocus * sinh(ks)^2
+  syz = @. focus * sinh(ks)^2 - defocus * sin(ks)^2
 
   @. vf.x  = vi.x  * cx + xp * s * sx
-  @. vf.px = vi.px * cx - k2 * p * vi.x * s * sx
+  @. vf.px = vi.px * cx - vi.x * p * k2 * s * sx
   @. vf.y  = vi.y  * cy + yp * s * sy
-  @. vf.py = vi.py * cy + k2 * p * vi.y * s * sy
+  @. vf.py = vi.py * cy + vi.y * p * k2 * s * sy
   @. vf.z  = (vi.z - (s / 4) * (xp^2 * (1 + sx2) + yp^2 * (1 + sy2)
                                 + k2 * vi.x^2 * (1 - sx2) - k2 * vi.y^2 * (1 - sy2))
-                   + (vi.x * xp * sxz - vi.y * yp * syz) / 2.)
+                   + (vi.x * xp * sxz - vi.y * yp * syz) / 2)
   @. vf.pz = vi.pz
 
   return vf
@@ -136,8 +133,12 @@ function trackQuadK!(vf, vi, s)
 
   @. vf.x  = vi.x + s * vi.px / p * ptr2 / (ps * (p + ps))
   @. vf.y  = vi.y + s * vi.py / p * ptr2 / (ps * (p + ps))
-  @. vf.z  = vi.z - s * (p / ps - (1/2) * ptr2 / p^2
-                         - p / (beta_ref * sqrt(p^2 + tilde_m^2)))
+  @. vf.z  = vi.z - s * ( (1.0 + v.pz)
+                         * ((v.px^2 + v.py^2) - v.pz * (2 + v.pz) / gamsqr_ref)
+                         / ( beta_ref * sqrt((1.0 + v.pz)^2 + tilde_m^2) * work[1]
+                             * (beta_ref * sqrt((1.0 + v.pz)^2 + tilde_m^2) + work[1])
+                           ) - (vi.px^2 + vi.py^2) / (2 * (1 + vi.pz)^2)
+                        )
   @. vf.px = vi.px
   @. vf.py = vi.py
   @. vf.pz = vi.pz
