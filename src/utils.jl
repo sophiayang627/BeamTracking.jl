3bf744bc3364c0e3831562f41336086c51a400d7
dtabell <dtabell@users.noreply.github.com>
2025-01-10 11:33:58 -0700

    Adjust format: delete trailing blank.

--- src/utils.jl
+++ src/utils.jl
@@ -166,7 +166,7 @@ function sinhcu(z::Number)
   end
 end
 
-# These will go in GTPSA 
+# These will go in GTPSA
 sincu(z::TPS) = sinc(z/pi)
 sinhcu(z::TPS) = sinhc(z/pi)
 sincu(z::GTPSA.TempTPS) = (GTPSA.div!(z,z,pi); return GTPSA.__t_sinc(z);)
